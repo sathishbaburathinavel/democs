a596fa37dd1b91a10b8e227d0c35718145072d7e
sathishbabur <sathishbabur@root.syncfusion.lan>
2016-11-08 02:32:51 -0800

    Added the demo project

---
+++ Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string assemblyName in GetAssemblyNames())
+            {
+                Console.WriteLine(assemblyName);
+            }
+        }
+        public static string[] GetAssemblyNames()
+        {
+            Assembly assembly = typeof(Program).Assembly;
+            AssemblyName[] referncedAssemblies = assembly.GetReferencedAssemblies();
+            string[] assemblies = new string[referncedAssemblies.Length];
+            int i = 0;
+            foreach (AssemblyName assemblyName in referncedAssemblies)
+            {
+                assemblies[i++] = assemblyName.FullName;
+            }
+            return assemblies;
+        }
+    }
+}
